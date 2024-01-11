6e7725124a967c41d31577ee9dad78de67a6a971
Fantomia <jonathan.andri@gmail.com>
2024-01-11 14:22:27 +0100

    Ajout d'un fonction permettant la mise à jour des classes pour les dots
    Ajout d'une fonction pour changer le contenu de la bannière
    
    Mis à jour de event listener pour actualiser le dot et la bannière actuelle en fonction du clique sur la flèche de gauche ou de droite
    
    Mise à jour du fichier css et html pour faire correspondre la classe tagLine associé au code js

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,70 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-const arrowLeft = document.querySelector('.arrow__left')
-const arrowRight = document.querySelector('.arrow__right')
-const bannerImg = document.querySelector('.banner__img');
+const arrowLeft = document.querySelector(".arrow__left");
+const arrowRight = document.querySelector(".arrow__right");
 
-arrowLeft.addEventListener('click', () => {
-    console.log('Tu as bien cliqué à gauche')
-})
+let indexItem = 0;
 
-arrowRight.addEventListener('click', () => {
-    console.log('Tu as bien cliqué à droite')
-})
+arrowLeft.addEventListener("click", () => {
+    if (indexItem - 1 >= 0) {
+        indexItem = indexItem - 1;
+		// vérifie que la décrémentation ne soit pas négative
+    } else {
+        indexItem = slides.length - 1;
+		// si la décrémentation est négative, alors revient à la dernière slide
+    }
+    updateDots(indexItem);
+    updateBanner();
+});
+
+arrowRight.addEventListener("click", () => {
+    if (indexItem + 1 < slides.length) {
+        indexItem = indexItem + 1;
+		// vérifie que l'incrémentation ne dépasse pas le nombre de slides
+    } else {
+        indexItem = 0;
+		// si l'incrémentation dépasse le nombre de slides, alors revient à la première slide
+    }
+    updateDots(indexItem);
+    updateBanner();
+});
+
+function updateDots(indexItem) {
+	const dots = document.querySelectorAll(".dot");
+    dots.forEach((dot, i) => {
+        if (i === indexItem) {
+            dot.classList.add("dot__selected");
+			// ajoute la classe "dot__selected" au dot associé à la diapositive 
+        } else {
+            dot.classList.remove("dot__selected");
+			// supprime la class "dot__selected" aux autres dots
+        }
+    });
+}
+
+function updateBanner() {
+	const bannerImg = document.querySelector(".banner__img");
+    bannerImg.src = slides[indexItem].image;
+	// change l'url source de l'image
+
+	const tagLine = document.querySelector(".tagLine");
+    tagLine.innerHTML = slides[indexItem].tagLine;
+	// change le texte en fonction de la tagLine associé à la slide actuelle
+}
